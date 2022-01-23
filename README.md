72c289264012e756e5ca2d0d6b42f6a64865f436
Marcin <marcinstaszakprogramista@protonmail.com>
2022-01-23 18:36:27 +0100

    README and data changed.

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
 # project1
 Simple Angular Project
 
-Project has simple functionality.
+Project has simple CRUD functionality.
 API providing data is simulated using RxJS library.
+App handles error states, empty state and loading state. When error occures, app let user to try to send API request by clicking "try again" button.
+In mocks.ts you can change data to display on list.
+App is responsive.
+Builded with strict mode.
 
 Node version: 16.3.2
 Angular 13
 Bootstrap 4
+TypeScript 4.5.5
 
 How to run:
 1. nvm install 16.3.2
--- src/app/services/mocks.ts
+++ src/app/services/mocks.ts
@@ -1,6 +1,7 @@
 import { DiaryItem } from "./data-structures/diary";
 
 export const mockedDiaryItems: DiaryItem[] = [
+    //basic data
     {
         id: 1,
         name: 'Michał',
@@ -35,7 +36,78 @@ export const mockedDiaryItems: DiaryItem[] = [
         surname: 'Paleciak',
         grade: 'IX',
         rating: 6
-    }
+    },
+    //more data
+    // {
+    //     id: 6,
+    //     name: 'Michał',
+    //     surname: 'Gonzowski',
+    //     grade: 'V',
+    //     rating: 2
+    // },
+    // {
+    //     id: 7,
+    //     name: 'Monika',
+    //     surname: 'Śmigło',
+    //     grade: 'VI',
+    //     rating: 2
+    // },
+    // {
+    //     id: 8,
+    //     name: 'Piotr',
+    //     surname: 'Fura',
+    //     grade: 'VII',
+    //     rating: 2
+    // },
+    // {
+    //     id: 9,
+    //     name: 'Seba',
+    //     surname: 'Kot',
+    //     grade: 'VIII',
+    //     rating: 6
+    // },
+    // {
+    //     id: 10,
+    //     name: 'Marcin',
+    //     surname: 'Paleciak',
+    //     grade: 'IX',
+    //     rating: 6
+    // },
+    // {
+    //     id: 11,
+    //     name: 'Michał',
+    //     surname: 'Gonzowski',
+    //     grade: 'V',
+    //     rating: 2
+    // },
+    // {
+    //     id: 12,
+    //     name: 'Monika',
+    //     surname: 'Śmigło',
+    //     grade: 'VI',
+    //     rating: 2
+    // },
+    // {
+    //     id: 13,
+    //     name: 'Piotr',
+    //     surname: 'Fura',
+    //     grade: 'VII',
+    //     rating: 2
+    // },
+    // {
+    //     id: 14,
+    //     name: 'Seba',
+    //     surname: 'Kot',
+    //     grade: 'VIII',
+    //     rating: 6
+    // },
+    // {
+    //     id: 15,
+    //     name: 'Marcin',
+    //     surname: 'Paleciak',
+    //     grade: 'IX',
+    //     rating: 6
+    // },
 ];
 
 export const mockedGradeItems: Array<string> = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
